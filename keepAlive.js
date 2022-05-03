40b925d4f03a60b88c54f2e86fc418072126f5fa
FinnyMarigold58 <59434139+FinnyMarigold58@users.noreply.github.com>
2022-05-02 22:46:10 -0500

    Add transcript webpage

--- keepAlive.js
+++ keepAlive.js
@@ -3,8 +3,18 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.static("public/transcripts"));
 //When the server is opened respond with a message
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get("/transcripts/:serverId/:ticketNumber", (req, res) => {
+  res.sendFile(
+    `${req.params.serverId}/${req.params.ticketNumber.split(".")[0]}.html`,
+    {
+      root: "./public/transcripts",
+    }
+  );
+});
 
 //Start the server
 app.listen(port, () =>
